3608f969b3adcbc97626cc3eaa4d0f84460553a1
Zorous <oussamabelhadi5@gmail.com>
2023-03-16 17:51:16 +0100

    integrating firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,3 +19,6 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 const analytics = getAnalytics(app);
+
+// Create a root reference
+export const storage = getStorage();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,29 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AddImg from "../img/add.png";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+const Register = () => {
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storage = getStorage();
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
 
-function Register() {
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+        },
+        () => {
+
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            await updateProfile(res.user,{
+              displayName,
+              photoURL: downloadURL
+            })
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
-      <span className="logo">Chat Room</span>
-      <span className="title">Register</span>
-        <form>
-          <input type="text" placeholder="your name"/>
-          <input type="email" placeholder="test@gmail.com"/>
-          <input type="password" placeholder="password"/>
-          <input style={{display:"none"}} type="file"  id="pic"  />
+        <span className="logo">Chat Room</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="your name" />
+          <input type="email" placeholder="test@gmail.com" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="pic" />
           <label htmlFor="pic">
-          <img src={AddImg}style={{width:"32px",height:"32px"}} alt="upload"/>
-          <span>Add an avatar</span>
+            <img
+              src={AddImg}
+              style={{ width: "32px", height: "32px" }}
+              alt="upload"
+            />
+            <span>Add an avatar</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>something went wrong</span>}
         </form>
-        <small>You already have an account? <Link to="/login">Login</Link></small>
+        <small>
+          You already have an account? <Link to="/login">Login</Link>
+        </small>
       </div>
     </div>
   );
-}
+};
 
 export default Register;
